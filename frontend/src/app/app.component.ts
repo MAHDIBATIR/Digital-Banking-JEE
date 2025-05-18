4fba5e9ae03fe7244490730ed02b7ba3452e9844
MAHDIBATIR <elbatirelmahdi@gmail.com>
2025-05-18 16:23:21 +0100

    Set up main application layout with navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'digital-banking-frontend';
+  title = 'Digital Banking';
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  handleLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
